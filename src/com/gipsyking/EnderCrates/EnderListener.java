516cc27c8df1a08905495cd209669409d0f89d7b
gipsy <ste3ls@gmail.com>
2014-09-07 15:44:39 +0200

    all nms stuff into NMSWrapper, cleanup

--- src/com/gipsyking/EnderCrates/EnderListener.java
+++ src/com/gipsyking/EnderCrates/EnderListener.java
@@ -2,76 +2,48 @@ package com.gipsyking.EnderCrates;
 
 import java.util.logging.Logger;
 
-import net.minecraft.server.v1_7_R1.EntityHorse;
-import net.minecraft.server.v1_7_R1.EntityPlayer;
-import net.minecraft.server.v1_7_R1.InventoryHorseChest;
-import net.minecraft.server.v1_7_R1.NBTTagCompound;
-import net.minecraft.server.v1_7_R1.NBTTagList;
-
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_7_R1.entity.CraftHorse;
-import org.bukkit.craftbukkit.v1_7_R1.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_7_R1.inventory.CraftInventory;
-import org.bukkit.craftbukkit.v1_7_R1.inventory.CraftInventoryHorse;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Horse;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.inventory.HorseInventory;
 import org.bukkit.inventory.ItemStack;
 
 public class EnderListener implements Listener{
 
-	private InventoryHandler ih;
 	
-	public EnderListener(InventoryHandler ih){
-		this.ih = ih;
-	}
-	
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
-	public void playerInteractEvent(PlayerInteractEvent event){
-		if (event.getClickedBlock() == null || event.getClickedBlock().getType() != Material.ENDER_CHEST
-				|| event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+	private static final int CRATE_SLOT = 2;
 
-		event.setCancelled(true);
-		ih.openInventory(event.getPlayer(), event.getClickedBlock());
-	}
-	
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
 	public void inventoryOpenEvent(InventoryOpenEvent event) {
-		if (event.getInventory() instanceof CraftInventoryHorse &&
-				event.getPlayer().getItemInHand().getType() != Material.ENDER_CHEST) {
-			EntityHorse horse = ((CraftHorse) event.getInventory().getHolder()).getHandle();
-			if (!horse.hasChest()) {
-				return;
-			}
-			
-			event.setCancelled(true);
-			
-			EntityPlayer h = ((CraftPlayer)event.getPlayer()).getHandle();
-			h.openContainer(horse.inventoryChest);
+		if (!(event.getInventory() instanceof HorseInventory)
+				|| !((Horse)event.getInventory().getHolder()).isCarryingChest()) {
+			return;
 		}
+		
+		event.setCancelled(true);
+		NMSWrapper.openDonkeyContainer((Player) event.getPlayer(), (Horse) event.getInventory().getHolder());
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
-	public void setChestEvent(PlayerInteractEntityEvent event) {
+	public void setDonkeyDoubleChestEvent(PlayerInteractEntityEvent event) {
 		if (!(event.getRightClicked() instanceof Horse) || event.getPlayer().getItemInHand().getType() != Material.ENDER_CHEST) {
 			return;
 		}
-		EntityHorse horse = ((CraftHorse) event.getRightClicked()).getHandle();
-		if (!horse.hasChest()) {
-			horse.setHasChest(true);
-			horse.inventoryChest = new InventoryHorseChest("Burrico", 54, horse);
-			event.getPlayer().getItemInHand().setAmount(event.getPlayer().getItemInHand().getAmount() - 1);
+		
+		Horse horse = (Horse) event.getRightClicked();
+		if (!horse.isCarryingChest()) {
 			event.setCancelled(true);
+			horse.setCarryingChest(true);
+			event.getPlayer().getItemInHand().setAmount(event.getPlayer().getItemInHand().getAmount() - 1);
+			NMSWrapper.setDonkeyChest(horse);
 		}
 	}
 	
@@ -79,28 +51,11 @@ public class EnderListener implements Listener{
 	public void onChunkLoaded(ChunkLoadEvent event) {
 		for (Entity entity: event.getChunk().getEntities()) {
 			if (entity instanceof Horse) {
-				EntityHorse horse = ((CraftHorse) entity).getHandle();
-				if (horse.hasChest()) {
-					NBTTagCompound nbttagcompound = new NBTTagCompound();
-					horse.b(nbttagcompound);
-					Logger.getGlobal().info("tag: " + nbttagcompound);
-					
-					ItemStack crate = ((CraftHorse) entity).getInventory().getItem(2);
-					
-					horse.inventoryChest = new InventoryHorseChest("Burrico", 54, horse);
-					
-					NMSWrapper.unzip(crate, ((CraftHorse) entity).getInventory());
-					
-//					NBTTagList nbttaglist = nbttagcompound.getList("Items", 10);
-//					for (int i = 0; i < nbttaglist.size(); ++i) {
-//		                NBTTagCompound nbttagcompound1 = nbttaglist.get(i);
-//		                int j = nbttagcompound1.getByte("Slot") & 255;
-//
-//		                if (j >= 2 && j < horse.inventoryChest.getSize()) {
-//		                	horse.inventoryChest.setItem(j, net.minecraft.server.v1_7_R1.ItemStack.createStack(nbttagcompound1));
-//		                }
-//		            }
-//					horse.a(tag);
+				Horse horse = (Horse) entity;
+				if (horse.isCarryingChest()) {
+					ItemStack crate = horse.getInventory().getItem(CRATE_SLOT);
+					NMSWrapper.setDonkeyChest(horse);
+					NMSWrapper.unzip(crate, horse.getInventory());
 				}
 			}
 		}
@@ -110,32 +65,19 @@ public class EnderListener implements Listener{
 	public void onChunkUnLoaded(ChunkUnloadEvent event) {
 		for (Entity entity: event.getChunk().getEntities()) {
 			if (entity instanceof Horse) {
-				CraftHorse horse = (CraftHorse) entity;
-//				EntityHorse horse = ((CraftHorse) entity).getHandle();
+				Horse horse = (Horse) entity;
 				if (horse.isCarryingChest()) {
-					ItemStack crate = NMSWrapper.crateItem(((CraftHorse) entity).getInventory().getContents());
-					horse.getInventory().clear();
-					horse.getInventory().setItem(2, crate);
-					
-					EntityHorse nmsHorse = horse.getHandle();
-					NBTTagCompound nbttagcompound = new NBTTagCompound();
-					nmsHorse.b(nbttagcompound);
-					Logger.getGlobal().info("tag: " + nbttagcompound);
+					this.saveDonkeyCrate(horse);
 				}
 			}
 		}
 	}
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
-	public void inventoryCloseEvent(InventoryCloseEvent event) {
-		ih.saveInventory(event.getView());
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
-	public void blockPlaceEvent(BlockPlaceEvent event) {
-		if (event.isCancelled() || event.getBlock().getType() != Material.CHEST) return;
-		
-		ih.restore(event.getBlock(), event.getItemInHand());
+
+	public static void saveDonkeyCrate(Horse horse) {
+		ItemStack crate = NMSWrapper.crateItem(horse.getInventory().getContents());
+		NMSWrapper.unsetDonkeyChest(horse);
+		horse.getInventory().setItem(CRATE_SLOT, crate);
+		Logger.getGlobal().info("Donkey saved crate");
 	}
 	
 }
--- src/com/gipsyking/EnderCrates/NMSWrapper.java
+++ src/com/gipsyking/EnderCrates/NMSWrapper.java
@@ -1,66 +1,23 @@
 package com.gipsyking.EnderCrates;
 
+import net.minecraft.server.v1_7_R1.EntityHorse;
+import net.minecraft.server.v1_7_R1.InventoryHorseChest;
 import net.minecraft.server.v1_7_R1.NBTTagCompound;
 import net.minecraft.server.v1_7_R1.NBTTagList;
 import net.minecraft.server.v1_7_R1.NBTTagString;
+
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.Chest;
+import org.bukkit.craftbukkit.v1_7_R1.entity.CraftHorse;
+import org.bukkit.craftbukkit.v1_7_R1.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_7_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class NMSWrapper {
 
-	public static void restore(Block block, ItemStack itemStack) {
-		net.minecraft.server.v1_7_R1.ItemStack nmsHandStack = CraftItemStack.asNMSCopy(itemStack);
-		NBTTagCompound tag = nmsHandStack.getTag();
-		if (tag == null || !tag.hasKey("contents")) {
-			return;
-		}
-		
-		nmsHandStack.setTag(null);
-		
-		Chest chest = (Chest) block.getState();
-		
-		NBTTagList contents = tag.getList("contents", 10);
-		
-		Inventory inventory = chest.getInventory();
-		for (int i = 0; i < contents.size(); i++) {
-			NBTTagCompound itemTag = contents.get(i);
-			
-			net.minecraft.server.v1_7_R1.ItemStack nmsStack = net.minecraft.server.v1_7_R1.ItemStack.createStack(itemTag);
-
-			inventory.addItem(CraftItemStack.asCraftMirror(nmsStack));
-		}
-		
-		// try to get rid of custom name, but this doesnt work
-		//TileEntityChest tileEntityChest = (TileEntityChest) ((CraftWorld) block.getWorld()).getTileEntityAt(block.getX(), block.getY(), block.getZ());
-		//tileEntityChest.a("");
-		
-	}
 	
-	public static void unzip(ItemStack itemStack, Inventory inventory) {
-		net.minecraft.server.v1_7_R1.ItemStack nmsHandStack = CraftItemStack.asNMSCopy(itemStack);
-		NBTTagCompound tag = nmsHandStack.getTag();
-		if (tag == null || !tag.hasKey("contents")) {
-			return;
-		}
-		
-		nmsHandStack.setTag(null);
-		
-		NBTTagList contents = tag.getList("contents", 10);
-		
-		for (int i = 0; i < contents.size(); i++) {
-			NBTTagCompound itemTag = contents.get(i);
-			
-			net.minecraft.server.v1_7_R1.ItemStack nmsStack = net.minecraft.server.v1_7_R1.ItemStack.createStack(itemTag);
-
-			inventory.addItem(CraftItemStack.asCraftMirror(nmsStack));
-		}
-		
-	}
-
 	public static ItemStack crateItem(Inventory inventory) {
 		return crateItem(inventory.getContents());
 	}
@@ -71,29 +28,52 @@ public class NMSWrapper {
 		nmsStack.setTag(tag);
 		
 		NBTTagCompound display = new NBTTagCompound();
-		display.setString("Name", "Crate");
+		display.setString("Name", "Donkey.zip");
 		NBTTagList lore = new NBTTagList();
+		lore.add(new NBTTagString("Error - place in first slot and reload chunk..."));
+		display.set("Lore", lore);
 		
 		NBTTagList contents = new NBTTagList();
 		
 		for (ItemStack invStack: stacks) {
-			if (invStack == null) {
-				continue;
-			}
-			lore.add(new NBTTagString(invStack.getType().toString() + " (" + invStack.getAmount() + ")"));
-			
-			
 			NBTTagCompound itemTag = new NBTTagCompound();
-			CraftItemStack.asNMSCopy(invStack).save(itemTag);
+			if (invStack != null) {
+				CraftItemStack.asNMSCopy(invStack).save(itemTag);
+			}
 						
 			contents.add(itemTag);
 		}
-		display.set("Lore", lore);
 		
 		tag.set("display", display);
 		tag.set("contents", contents);
 		
 		return CraftItemStack.asCraftMirror(nmsStack);
+	}
+
+	public static void unzip(ItemStack itemStack, Inventory inventory) {
+		net.minecraft.server.v1_7_R1.ItemStack nmsHandStack = CraftItemStack.asNMSCopy(itemStack);
+		if (nmsHandStack == null) {
+			return;
+		}
+		NBTTagCompound tag = nmsHandStack.getTag();
+		if (tag == null || !tag.hasKey("contents")) {
+			return;
+		}
+		
+		nmsHandStack.setTag(null);
+		
+		NBTTagList contents = tag.getList("contents", 10);
+		
+		for (int i = 0; i < contents.size(); i++) {
+			NBTTagCompound itemTag = contents.get(i);
+			
+			net.minecraft.server.v1_7_R1.ItemStack nmsStack = net.minecraft.server.v1_7_R1.ItemStack.createStack(itemTag);
+			
+			if (!itemTag.isEmpty()) {
+				inventory.setItem(i, CraftItemStack.asCraftMirror(nmsStack));
+			}
+		}
+		
 	}
 
 	public static ItemStack emptyCrateItem() {
@@ -108,5 +88,23 @@ public class NMSWrapper {
 		display.set("Lore", lore);
 		
 		return CraftItemStack.asCraftMirror(nmsStack);
+	}
+
+	public static void openDonkeyContainer(Player player, Horse horse) {
+		
+		((CraftPlayer)player).getHandle().openContainer(((CraftHorse)horse).getHandle().inventoryChest);
+	}
+
+	public static void setDonkeyChest(Horse horse) {
+		EntityHorse handle = ((CraftHorse)horse).getHandle();
+		handle.inventoryChest = new InventoryHorseChest("Burrico", 54, handle);
+	}
+
+	public static void unsetDonkeyChest(Horse horse) {
+		horse.getInventory().clear();
+		
+		EntityHorse handle = ((CraftHorse)horse).getHandle();
+		handle.inventoryChest = null;
+		handle.loadChest();
 	}
 }
